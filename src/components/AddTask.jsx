8c92270a69de20c1ecaee0998daa5020a46b974d
luispfatdo <luis.paulo.tdo@gmail.com>
2025-12-29 10:00:19 -0300

    Limpando os campos após adicionar um novo item.

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -21,7 +21,11 @@ function AddTask({ onAddTaskSubmit }) {
         onChange={(event) => setDescription(event.target.value)}
       />
       <button
-        onClick={() => onAddTaskSubmit(title, description)}
+        onClick={() => {
+          onAddTaskSubmit(title, description);
+          setTitle("");
+          setDescription("");
+        }}
         className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
       >
         Adicionar
