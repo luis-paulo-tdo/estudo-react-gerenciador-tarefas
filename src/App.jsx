1a830db2299af0585708f54423065d60b87ea037
luispfatdo <luis.paulo.tdo@gmail.com>
2025-12-29 04:23:57 -0300

    Estilização inicial da tela principal e do título.

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,14 @@ function App() {
   const [message, setMessage] = useState("Olá, Mundo!");
 
   return (
-    <div>
-      <h1 className="text-red-500">Task Manager</h1>
-      <AddTask />
-      <Tasks />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Task Manager
+        </h1>
+        <AddTask />
+        <Tasks />
+      </div>
     </div>
   );
 }
